ad168129934b8ace3cde8a6aba7010408fed42b6
ZkEdison <214236543@qq.com>
2017-04-23 23:02:19 +0800

    minit

---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div class="">
+    Clicked: {{$store.state.count}} times, count is {{evenOrodd}}
+    <hr>
+    <button type="button" @click="increment">+</button>
+    <button type="button" @click="decrement">-</button>
+    <br>
+    <button type="button" @click="incrementIfOdd">Increment if odd</button>
+    <br>
+    <button type="button" @click="incrementAsync">Increment async</button>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  computed: mapGetters([
+    'evenOrodd'
+  ]),
+  methods: mapActions([
+    'increment',
+    'decrement',
+    'incrementIfOdd',
+    'incrementAsync'
+  ])
+}
+</script>
+
+<style lang="css">
+</style>
